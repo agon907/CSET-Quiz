6d6301d4722273b0a459d9f4aef2e00a70471baa
Ahimelec Gonzalez <148896308+agon907@users.noreply.github.com>
2023-11-08 13:44:54 -0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,233 @@
+const questions = [
+    {
+        question: "How many legs does a spider have?",
+        answers: [
+            {text: "4", correct: false},
+            {text: "5", correct: false},
+            {text: "7", correct: false},
+            {text: "8", correct: true},
+        ]
+    },
+    {
+        question: "What is the color of an emerald?",
+        answers: [
+            {text: "red", correct: false},
+            {text: "green", correct: true},
+            {text: "blue", correct: false},
+            {text: "yellow", correct: false},
+        ]
+    },
+    {
+        question: "Which ocean is off the coast of California?",
+        answers: [
+            {text: "The Pacific", correct: true},
+            {text: "The Atlantic", correct: false},
+            {text: "The Arctic", correct: false},
+            {text: "The Indian", correct: false},
+        ]
+    },
+    {
+        question: "On which holidy do you go trick-or-treating?",
+        answers: [
+            {text: "Christmas", correct: false},
+            {text: "Easter", correct: false},
+            {text: "Thanksgiving", correct: false},
+            {text: "Halloween", correct: true},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer=>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    });
+}
+function resetState(){
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer (e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display ="block";
+}
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+function handleNextButton(){
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = [
+        {
+            question: "How many legs does a spider have?",
+            answers: [
+                {text: "4", correct: false},
+                {text: "5", correct: false},
+                {text: "7", correct: false},
+                {text: "8", correct: true},
+            ]
+        },
+        {
+            question: "What is the color of an emerald?",
+            answers: [
+                {text: "red", correct: false},
+                {text: "green", correct: true},
+                {text: "blue", correct: false},
+                {text: "yellow", correct: false},
+            ]
+        },
+        {
+            question: "Which ocean is off the coast of California?",
+            answers: [
+                {text: "The Pacific", correct: true},
+                {text: "The Atlantic", correct: false},
+                {text: "The Arctic", correct: false},
+                {text: "The Indian", correct: false},
+            ]
+        },
+        {
+            question: "On which holidy do you go trick-or-treating?",
+            answers: [
+                {text: "Christmas", correct: false},
+                {text: "Easter", correct: false},
+                {text: "Thanksgiving", correct: false},
+                {text: "Halloween", correct: true},
+            ]
+        }
+    ];
+    
+    const questionElement = document.getElementById("question");
+    const answerButtons = document.getElementById("answer-buttons");
+    const nextButton = document.getElementById("next-btn");
+    
+    let currentQuestionIndex = 0;
+    let score = 0;
+    
+    function startQuiz(){
+        currentQuestionIndex = 0
+        score = 0;
+        nextButton.innerHTML = "Next";
+        showQuestion();
+    }
+    function showQuestion(){
+        resetState();
+        let currentQuestion = questions[currentQuestionIndex];
+        let questionNo = currentQuestionIndex + 1;
+        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+        currentQuestion.answers.forEach(answer=>{
+            const button = document.createElement("button");
+            button.innerHTML = answer.text;
+            button.classList.add("btn");
+            answerButtons.appendChild(button);
+            if(answer.correct){
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener("click",selectAnswer);
+        });
+    }
+    function resetState(){
+        nextButton.style.display = "none";
+        while (answerButtons.firstChild){
+            answerButtons.removeChild(answerButtons.firstChild);
+        }
+    }
+    function selectAnswer (e){
+        const selectedBtn = e.target;
+        const isCorrect = selectedBtn.dataset.correct === "true";
+        if (isCorrect){
+            selectedBtn.classList.add("correct");
+            score++;
+        }else{
+            selectedBtn.classList.add("incorrect");
+        }
+        Array.from(answerButtons.children).forEach(button => {
+            if (button.dataset.correct === "true"){
+                button.classList.add("correct");
+            }
+            button.disabled = true;
+        });
+        nextButton.style.display ="block";
+    }
+    function showScore(){
+        resetState();
+        questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+        nextButton.innerHTML = "Play Again";
+        nextButton.style.display = "block";
+    }
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length){
+            showQuestion();
+        }else{
+            showScore();
+        }
+    }
+    nextButton.addEventListener("click",()=>{
+        if(currentQuestionIndex < questions.length){
+            handleNextButton();
+        }else{
+            startQuiz();
+        }
+    })
+    
+    startQuiz();
+  });
